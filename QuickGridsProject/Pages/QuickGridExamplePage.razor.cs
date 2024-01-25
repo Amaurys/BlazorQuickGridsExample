3dc5a44f2f801f409ff679aa5a8e97e55e60663a
Amaurys Sanchez <20170327@unapec.edu.do>
2024-01-25 00:13:44 -0400

    Add project files.

---
+++ QuickGridsProject/Pages/QuickGridExamplePage.razor.cs
@@ -0,0 +1,26 @@
+﻿using QuickGridsProject.Models;
+
+namespace QuickGridsProject.Pages
+{
+    public partial class QuickGridExamplePage
+    {
+        const string test = "a";
+        private IQueryable<Car> CreateCarList()
+        {
+            List<Car> myCarList = [];
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var car = new Car()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Car {i}",
+                    Cost = i * 77 + 105
+                };
+                myCarList.Add(car);
+            }
+
+            return myCarList.AsQueryable();
+        }
+    }
+}
